48a98ce356264c09c7b0a569cb926f6718bdb22c
Claudio Mauri <mauri.claudio02@gmail.com>
2022-05-25 12:03:43 +0200

    listNightmareSeries

--- js/modulo-api.js
+++ js/modulo-api.js
@@ -1,8 +1,19 @@
 const URL_LIST_NIGHTMARE = 
     "http://www.omdbapi.com/?apikey=96fcfbd7&s=nightmare";
+const URL_LIST_NIGHTMARE_SERIES = 
+    "http://www.omdbapi.com/?apikey=96fcfbd7&s=nightmare&&type=series";
 
 export const listNightmare = () => {
     fetch(URL_LIST_NIGHTMARE)
+        .then((response) => response.json())
+        .then((results) => {
+            const movies = results.Search;
+            console.log(movies);
+        });
+};
+
+export const listNightmareSeries = () => {
+    fetch(URL_LIST_NIGHTMARE_SERIES)
         .then((response) => response.json())
         .then((results) => {
             const movies = results.Search;
