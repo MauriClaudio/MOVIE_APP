42ed3ec1678b30d26233b2469d0bad40af7d0afe
Claudio Mauri <mauri.claudio02@gmail.com>
2022-06-07 17:57:16 +0200

    inserimento di api in movie app

---
+++ sito/js/config.js
@@ -0,0 +1,9 @@
+const API_KEY = "96fcfbd7";
+export const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
+
+export const CONTENT_TYPE = {
+    MOVIE: "movie",
+    SERIES: "series",
+    EPISODE: "game",
+    GAME: "game"
+}
