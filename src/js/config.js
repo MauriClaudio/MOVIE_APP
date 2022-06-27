00404d118296e68cf0967401b7991da05fb21736
Claudio Mauri <mauri.claudio02@gmail.com>
2022-06-27 22:42:23 +0200

    prova azure 3

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 const API_KEY = "96fcfbd7";
-export const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
+export const BASE_URL = `https://www.omdbapi.com/?apikey=${API_KEY}&`;
 
 export const CONTENT_TYPE = {
     MOVIE: "movie",
